70bd022c314abd83813a023ba46676915cc8cbbc
Ryan <leonheart94@gmail.com>
2017-12-22 18:04:18 +0800

    Database Migrations & Readme

--- ArticlesAPIServer/database/migrations/2017_12_22_071220_create_articles_table.php
+++ ArticlesAPIServer/database/migrations/2017_12_22_071220_create_articles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateArticlesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('name');
-            $table->string('privateKey');
-            $table->string('publicKey');
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('content');
+            $table->text('remark');
+            $table->integer('user_record')->unsigned();
+            $table->integer('user_modified')->unsigned();
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('articles');
     }
 }
---
+++ ConversationAPIServer/database/migrations/2017_12_22_091300_create_keywords_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKeywordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('keywords', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('indonesian_text');
+            $table->text('chinese_text');
+            $table->string('voice_link');
+            $table->int('daily_usage_limit');
+            $table->integer('user_record')->unsigned();
+            $table->integer('user_modified')->unsigned();
+            $table->text('remark');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('keywords');
+    }
+}
