69a74954b3d0c1aaef687dc8581062e5c3a2af8c
yuliagiarto <34241242+yuliagiarto@users.noreply.github.com>
2018-01-01 21:44:37 +0800

    insert conversation

--- FrontendServer/app/Http/Controllers/ConversationController.php
+++ FrontendServer/app/Http/Controllers/ConversationController.php
@@ -44,5 +44,73 @@ class ConversationController extends Controller {
         curl_close($cSession);
         return view('conversation', ['categories' => $categories, 'conversations' => $conversations]);
     }
+    
+    public function insertConversation(Request $request) {
+        $url = config('app.conversationServer');
+
+        $ch = curl_init();
+        $data = $request->all();
+        $privKey = auth()->user()->privateKey;
+        $pubKey = auth()->user()->publicKey;
+
+//        $post = [
+//            'title' => $data['title'],
+//            'content' => $data['content'],
+//            'user' => $data['user'],
+//            'category_id' => $data['category_id'],
+//        ];
+        $post = [
+            'indonesian_text' => $data['indonesian_text'],
+            'chinese_text' => $data['chinese_text'],
+            'content' => $data['content'],
+            'user' => $data['user'],
+            'category_id' => $data['category_id'],
+        ];
+
+        $payload = json_encode($post);
+        openssl_private_encrypt($payload, $encrypted, $privKey);
+        // without serialization
+        if (isset($data['voice'])) {
+            if (count($data['voice']) > 0) {
+                foreach ($data['voice'] as $index => $voice) {
+                    $path = $voice->move('uploads/tmp', uniqid() . $voice->getClientOriginalName());
+                    $postVoice['voice[' . $index . ']'] = new \CURLFile(realpath($path));
+                }
+            }
+        }
+        $postVoice['payload'] = $encrypted;
+
+
+        // file with serialization
+        // if (count($data['images']) > 0){
+        //     foreach($data['images'] as $index=>$image){
+        //         $path = $image->move('uploads/tmp', uniqid() . $image->getClientOriginalName());
+        //         $post['images['.$index.']'] = realpath($path);
+        //     }    
+        // }
+
+        $test = json_encode($post);
+//        
+//        $secret = auth()->user()->secret;
+//        
+//        $post['signature'] = sha1($payload.$secret);
+
+        curl_setopt($ch, CURLOPT_URL, $url . 'keyword/insert');
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postVoice);
+
+        // in real life you should use something like:
+        // curl_setopt($ch, CURLOPT_POSTFIELDS, 
+        //          http_build_query(array('postvar1' => 'value1')));
+        // receive server response ...
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $server_output = curl_exec($ch);
+
+        dd($server_output);
+        curl_close($ch);
+
+        return redirect('showConversation');
+    }
 
 }
