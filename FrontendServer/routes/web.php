85f1fece5dabb9e5f6a61ff1d6e6e52ae6eeb04f
Ryan <leonheart94@gmail.com>
2018-01-01 22:02:52 +0800

    commit

--- FrontendServer/routes/web.php
+++ FrontendServer/routes/web.php
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-
-Auth::routes();
-
-Route::any('/', 'HomeController@index')->name('home');
-Route::any('/single/{id}', 'HomeController@single')->name('single');
-Route::any('/news/{category}', 'HomeController@news')->name('news');
-//Route::any('/home2', 'HomeController@index2')->name('home2');
-Route::any('/showArticle', 'ArticleController@showArticle')->name('showArticle');
-Route::any('/showConversation', 'ConversationController@showConversation')->name('showConversation');
-Route::any('/article/insert','ArticleController@insertArticle');
-Route::any('/article/delete','ArticleController@deleteArticle');
-=======
 <?php
 
 /*
@@ -54,5 +25,4 @@ Route::any('/showArticle', 'ArticleController@showArticle')->name('showArticle')
 Route::any('/showConversation', 'ConversationController@showConversation')->name('showConversation');
 Route::any('/article/insert','ArticleController@insertArticle');
 Route::any('/conversation/insert','ConversationController@insertConversation');
-
->>>>>>> 69a74954b3d0c1aaef687dc8581062e5c3a2af8c
+Route::any('/article/delete','ArticleController@deleteArticle');
